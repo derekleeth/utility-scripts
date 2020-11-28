8dfa04b6c968b223955cc20bb87a08111f1cf752
Derek Leeth <derekleeth@gmail.com>
2020-11-28 17:49:13 -0500

    plex/remove_lonely_collections: create

---
+++ plex/remove_lonely_collections.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from plexapi.server import PlexServer
+import urllib3
+import configparser
+
+urllib3.disable_warnings()
+
+config_parser = configparser.ConfigParser()
+config_parser.read(r"../scripts.config")
+PLEX_CONFIG = config_parser['PLEX']
+
+LIBRARIES = ["Movies", "TV Shows"]
+
+PLEX_URL = config_parser.get('PLEX', 'PLEX_URL')
+PLEX_TOKEN = config_parser.get('PLEX', 'PLEX_TOKEN')
+## CODE BELOW ##
+
+sess = requests.Session()
+sess.verify = False
+plex = PlexServer(PLEX_URL, PLEX_TOKEN, session=sess)
+
+for library in LIBRARIES:
+    for collection in plex.library.section(library).search(libtype="collection"):
+        if collection.childCount < 2:
+            collection.delete()
+            # Other options
+            # collection.modeUpdate("hideItems")
+            #collection.modeUpdate("hide")
+            print("{0} ({1} items)".format(collection, collection.childCount))
