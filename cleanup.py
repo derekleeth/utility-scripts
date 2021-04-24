26850a50b8f7a897c9f4b0c2070d8573a128faf1
Derek Leeth <derekleeth@gmail.com>
2021-04-24 11:49:16 -0400

    cleanup:

--- cleanup.py
+++ cleanup.py
@@ -3,6 +3,7 @@
 import os
 import logging
 import logging.handlers
+import syslog
 
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.DEBUG)
@@ -12,6 +13,10 @@ handler = logging.handlers.SysLogHandler()
 logger.addHandler(handler)
 
 logger.debug('this is debug')
+logger.info('this is debug')
+
+syslog.syslog(syslog.LOG_INFO, "Test message")
+
 
 zero_file_cleanup_dirs = ["/mnt/storage/media/tv/", "/mnt/storage/media/movies/"]
 media_cleanup_dirs = ["/mnt/storage/media/deleted_files/TV/", "/mnt/storage/media/deleted_files/Movies"]
@@ -40,6 +45,7 @@ def cleanup_deleted_files():
         for dirpath, dirs, files in os.walk(cleanup_dir):
             for file in files:  
                 path = os.path.join(dirpath, file)
+                print(path)
                 
 
 if __name__ == '__main__':
